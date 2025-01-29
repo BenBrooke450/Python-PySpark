fffcf209c7c1bb46faefc4f9ae36b33ccc141654
BenBrooke450 <bensir@mail.com>
2025-01-30 00:29:59 +0100

    Basic

--- Pyspark Practice/machinelearningplus/PySpark Exercises 1 - 50.py
+++ Pyspark Practice/machinelearningplus/PySpark Exercises 1 - 50.py
@@ -47,7 +47,7 @@ df = df.withColumn("index",rank().over(w))
 
 df.show()
 
-"""
+
 +-------+-----+-----+
 |   Name|Value|index|
 +-------+-----+-----+
@@ -55,7 +55,7 @@ df.show()
 |    Bob|    2|    2|
 |Charlie|    3|    3|
 +-------+-----+-----+
-"""
+
 
 
 
@@ -142,6 +142,7 @@ B = [4, 5, 6, 7, 8]
 B = spark.createDataFrame([(x,) for x in B],["second_value"])
 
 df = A.subtract(B).show()
+
 +-----+
 |value|
 +-----+
@@ -254,6 +255,7 @@ print(Age)
 
 qs = df.approxQuantile("Age", [0.25, 0.5, 0.75], 0.0)
 print(qs)
+
 
 
 
@@ -377,7 +379,7 @@ df = df.withColumn("row_idx",row_number().over(Window.orderBy(monotonically_incr
 
 df = df.withColumn("row_idx",when(col("row_idx")<3,col("row_idx")).otherwise("other")).show()
 
-"""
+
 +---------+-----+-------+
 |      job|Count|row_idx|
 +---------+-----+-------+
@@ -386,7 +388,7 @@ df = df.withColumn("row_idx",when(col("row_idx")<3,col("row_idx")).otherwise("ot
 |   Doctor|    1|  other|
 +---------+-----+-------+
 
-"""
+
 
 
 
@@ -396,7 +398,444 @@ df = df.withColumn("row_idx",when(col("row_idx")<3,col("row_idx")).otherwise("ot
 
 ###############################################################################
 
-#9
+#9 How to Drop rows with NA values specific to a particular column?
+
+
+# Initialize Spark session
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName('Spark Playground').getOrCreate()
+
+df = spark.createDataFrame([
+("A", 1, None),
+("B", None, "123" ),
+("B", 3, "456"),
+("D", None, None),
+], ["Name", "Value", "id"])
+
+df.na.drop().show()
+
+
++----+-----+---+
+|Name|Value| id|
++----+-----+---+
+|   B|    3|456|
++----+-----+---+
+
+
+
+
+###############################################################################
+
+
+#10 How to rename columns of a PySpark DataFrame using two lists –  one containing the old column names and the other containing the new column names?
+
+"""
+# suppose you have the following DataFrame
+df = spark.createDataFrame([(1, 2, 3), (4, 5, 6)], ["col1", "col2", "col3"])
+
+# old column names
+old_names = ["col1", "col2", "col3"]
+
+# new column names
+new_names = ["new_col1", "new_col2", "new_col3"]
+
+df.show()
++----+----+----+
+|col1|col2|col3|
++----+----+----+
+| 1| 2| 3|
+| 4| 5| 6|
++----+----+----+
+"""
+
+# Initialize Spark session
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import max, count, col, rank, row_number, monotonically_increasing_id, when
+
+spark = SparkSession.builder.appName('Spark Playground').getOrCreate()
+
+# suppose you have the following DataFrame
+df = spark.createDataFrame([(1, 2, 3), (4, 5, 6)], ["col1", "col2", "col3"])
+
+# old column names
+old_names = ["col1", "col2", "col3"]
+
+# new column names
+new_names = ["new_col1", "new_col2", "new_col3"]
+
+df.withColumnRenamed("col1", "new_col1")
+    .withColumnRenamed("col2", "new_col2")
+    .withColumnRenamed("col3","new_col3").show()
+
+
++--------+--------+--------+
+|new_col1|new_col2|new_col3|
++--------+--------+--------+
+|       1|       2|       3|
+|       4|       5|       6|
++--------+--------+--------+
+
+
+###############################################################################
+
+
+#11. How to create contigency table?
+
+"""
+# Example DataFrame
+data = [("A", "X"), ("A", "Y"), ("A", "X"), ("B", "Y"), ("B", "X"), ("C", "X"), ("C", "X"), ("C", "Y")]
+df = spark.createDataFrame(data, ["category1", "category2"])
+
+df.show()
+"""
+
+# Initialize Spark session
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import max, count, col, rank, row_number, monotonically_increasing_id, when
+
+spark = SparkSession.builder.appName('Spark Playground').getOrCreate()
+
+data = [("A", "X"), ("A", "Y"), ("A", "X"), ("B", "Y"), ("B", "X"), ("C", "X"), ("C", "X"), ("C", "Y")]
+
+df = spark.createDataFrame(data, ["c1", "c2"])
+
+df.crosstab("c1", "c2").sort("c1_c2").show()
+
++-----+---+---+
+|c1_c2|  X|  Y|
++-----+---+---+
+|    A|  2|  1|
+|    B|  1|  1|
+|    C|  2|  1|
++-----+---+---+
+
+
+
+
+###############################################################################
+
+
+#13 How to find the numbers that are multiples of 3 from a column?
+
+"""
+from pyspark.sql.functions import rand
+
+# Generate a DataFrame with a single column "id" with 10 rows
+df = spark.range(10)
+
+# Generate a random float between 0 and 1, scale and shift it to get a random integer between 1 and 10
+df = df.withColumn("random", ((rand(seed=42) * 10) + 1).cast("int"))
+
+# Show the DataFrame
+df.show()
++---+------+
+| id|random|
++---+------+
+| 0| 7|
+| 1| 6|
+| 2| 9|
+| 3| 7|
+| 4| 3|
+| 5| 8|
+| 6| 9|
+| 7| 8|
+| 8| 3|
+| 9| 8|
++---+------+
+"""
+
+
+
+# Initialize Spark session
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName('Spark Playground').getOrCreate()
+
+from pyspark.sql.functions import rand
+
+# Generate a DataFrame with a single column "id" with 10 rows
+df = spark.range(10)
+
+# Generate a random float between 0 and 1, scale and shift it to get a random integer between 1 and 10
+df = df.withColumn("random", ((rand(seed=42) * 10) + 1).cast("int"))
+
+df.filter(col("random")%2==0).show()
+
+
++---+------+
+| id|random|
++---+------+
+|  1|     6|
++---+------+
+
+
+
+###############################################################################
+
+
+#14 How to extract items at given positions from a column?
+
+"""
+from pyspark.sql.functions import rand
+
+# Generate a DataFrame with a single column "id" with 10 rows
+df = spark.range(10)
+
+# Generate a random float between 0 and 1, scale and shift it to get a random integer between 1 and 10
+df = df.withColumn("random", ((rand(seed=42) * 10) + 1).cast("int"))
+
+# Show the DataFrame
+df.show()
+
+pos = [0, 4, 8, 5]
++---+------+
+| id|random|
++---+------+
+| 0| 7|
+| 1| 6|
+| 2| 9|
+| 3| 7|
+| 4| 3|
+| 5| 8|
+| 6| 9|
+| 7| 8|
+| 8| 3|
+| 9| 8|
++---+------+
+"""
+
+
+
+# Initialize Spark session
+from pyspark.sql import SparkSession
+from pyspark.sql import Window, types
+from pyspark.sql import Row
+from pyspark.sql.functions import max, count, col, rank, row_number, monotonically_increasing_id, when
+
+spark = SparkSession.builder.appName('Spark Playground').getOrCreate()
+
+from pyspark.sql.functions import rand
+
+# Generate a DataFrame with a single column "id" with 10 rows
+df = spark.range(10)
+
+# Generate a random float between 0 and 1, scale and shift it to get a random integer between 1 and 10
+df = df.withColumn("random", ((rand(seed=42) * 10) + 1).cast("int"))
+
+# Show the DataFrame
+df.show()
+
+pos = [0, 4, 8, 5]
+
+print(df.collect()[0])
+#Row(id=0, random=7)
+
+print(df.collect()[4])
+#Row(id=4, random=7)
+
+print(df.collect()[8])
+#Row(id=8, random=3)
+
+print(df.collect()[5])
+#Row(id=5, random=9)
+
+
+
+
+
+###############################################################################
+
+#15 How to stack two DataFrames vertically ?
+
+"""
+# Create DataFrame for region A
+df_A = spark.createDataFrame([("apple", 3, 5), ("banana", 1, 10), ("orange", 2, 8)], ["Name", "Col_1", "Col_2"])
+df_A.show()
+
+# Create DataFrame for region B
+df_B = spark.createDataFrame([("apple", 3, 5), ("banana", 1, 15), ("grape", 4, 6)], ["Name", "Col_1", "Col_3"])
+df_B.show()
++------+-----+-----+
+| Name|Col_1|Col_2|
++------+-----+-----+
+| apple| 3| 5|
+|banana| 1| 10|
+|orange| 2| 8|
++------+-----+-----+
+
++------+-----+-----+
+| Name|Col_1|Col_3|
++------+-----+-----+
+| apple| 3| 5|
+|banana| 1| 15|
+| grape| 4| 6|
++------+-----+-----+
+"""
+
+
+
+
+# Initialize Spark session
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName('Spark Playground').getOrCreate()
+
+# Create DataFrame for region A
+df_A = spark.createDataFrame([("apple", 3, 5), ("banana", 1, 10), ("orange", 2, 8)], ["Name", "Col_1", "Col_2"])
+
+# Create DataFrame for region B
+df_B = spark.createDataFrame([("apple", 3, 5), ("banana", 1, 15), ("grape", 4, 6)], ["Name", "Col_1", "Col_3"])
+
+df_A.union(df_B).show()
+
+
++------+-----+-----+
+|  Name|Col_1|Col_2|
++------+-----+-----+
+| apple|    3|    5|
+|banana|    1|   10|
+|orange|    2|    8|
+| apple|    3|    5|
+|banana|    1|   15|
+| grape|    4|    6|
++------+-----+-----+
+
+
+
+
+
+
+
+
+
+###############################################################################
+
+
+#17 How to convert the first character of each element in a series to uppercase?
+
+"""
+# Suppose you have the following DataFrame
+data = [("john",), ("alice",), ("bob",)]
+df = spark.createDataFrame(data, ["name"])
+
+df.show()
++-----+
+| name|
++-----+
+| john|
+|alice|
+| bob|
++-----+
+"""
+
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import max, count, col, rank, row_number, monotonically_increasing_id, when, upper
+
+spark = SparkSession.builder.appName('Spark Playground').getOrCreate()
+
+data = [("john",), ("alice",), ("bob",)]
+
+df = spark.createDataFrame(data, ["name"])
+
+list1 = []
+
+for i, n in enumerate(df.collect()):
+    list1.append((i + 1, n[0][0].upper() + n[0][1:]))
+
+df = spark.createDataFrame(list1, ["number", "name"])
+
+df.show()
+
++------+-----+
+|number| name|
++------+-----+
+|     1| John|
+|     2|Alice|
+|     3|  Bob|
++------+-----+
+
+
+
+
+
+###############################################################################
+
+
+#18 How to compute summary statistics for all columns in a dataframe
+
+
+"""
+# For the sake of example, we'll create a sample DataFrame
+data = [('James', 34, 55000),
+('Michael', 30, 70000),
+('Robert', 37, 60000),
+('Maria', 29, 80000),
+('Jen', 32, 65000)]
+
+df = spark.createDataFrame(data, ["name", "age" , "salary"])
+
+df.show()
++-------+---+------+
+| name|age|salary|
++-------+---+------+
+| James| 34| 55000|
+|Michael| 30| 70000|
+| Robert| 37| 60000|
+| Maria| 29| 80000|
+| Jen| 32| 65000|
++-------+---+------+
+"""
+
+
+# Initialize Spark session
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import max, count, col, rank, row_number, monotonically_increasing_id, when, upper
+
+spark = SparkSession.builder.appName('Spark Playground').getOrCreate()
+
+data = [('James', 34, 55000),
+('Michael', 30, 70000),
+('Robert', 37, 60000),
+('Maria', 29, 80000),
+('Jen', 32, 65000)]
+
+df = spark.createDataFrame(data, ["name", "age" , "salary"])
+
+df.summary().show()
+
++-------+------+-----------------+-----------------+
+|summary|  name|              age|           salary|
++-------+------+-----------------+-----------------+
+|  count|     5|                5|                5|
+|   mean|  NULL|             32.4|          66000.0|
+| stddev|  NULL|3.209361307176242|9617.692030835671|
+|    min| James|               29|            55000|
+|    25%|  NULL|               30|            60000|
+|    50%|  NULL|               32|            65000|
+|    75%|  NULL|               34|            70000|
+|    max|Robert|               37|            80000|
++-------+------+-----------------+-----------------+
+
+
+
+###############################################################################
+
+#19
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
