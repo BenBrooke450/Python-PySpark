fbc1336a882f003d6081cc60a08ca04bb57de37e
BenBrooke450 <bensir@mail.com>
2025-04-29 17:45:50 +0200

    Basic

--- Pyspark Practice/sparkplayground/30.DAY(partitionBy()).py
+++ Pyspark Practice/sparkplayground/30.DAY(partitionBy()).py
@@ -68,3 +68,73 @@ df.show()
 
 
 
+
+
+
+
+
+
+
+
+
+from pyspark.sql import SparkSession
+from pyspark.sql import Row
+from pyspark.sql.functions import col, when, max, lit, lag, concat_ws
+
+from pyspark.sql.window import Window
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType
+
+# Initialize SparkSession
+
+spark = SparkSession.builder.appName("ExtractNestedData").getOrCreate()
+
+data = [
+    Row(name = "Ben",A='apple', B='cat', C='red', D=None, E='high'),
+    Row(name = "Ben",A='banana', B=None, C='blue', D='circle', E='medium'),
+    Row(name = "Ben",A='cherry', B='dog', C=None, D='triangle', E='low'),
+    Row(name = "Ana",A='cherry', B='elephant', C='green', D='square', E='high'),
+    Row(name = "Ana",A='date', B=None, C='yellow', D=None, E='low'),
+    Row(name = "Marta",A='cherry', B='fox', C='purple', D='hexagon', E='low'),
+    Row(name = "Ana",A='fig', B='goat', C=None, D='octagon', E='high'),
+    Row(name = "Ben",A='grape', B='horse', C='orange', D='rectangle', E='medium'),
+    Row(name = "Marta",A='apple', B='iguana', C='pink', D=None, E='low'),
+    Row(name = "Marta",A='apple', B='jaguar', C='black', D='diamond', E='low')
+]
+
+df = spark.createDataFrame(data)
+
+window_spec = Window.partitionBy("name").orderBy("A")
+
+df.withColumn("prev_furit", lag("A").over(window_spec)).withColumn("prev_furit", concat_ws(" - ","name","prev_furit")).show()
+"""
++-----+------+--------+------+---------+------+-------------+
+| name|     A|       B|     C|        D|     E|   prev_furit|
++-----+------+--------+------+---------+------+-------------+
+|  Ana|cherry|elephant| green|   square|  high|          Ana|
+|  Ana|  date|    NULL|yellow|     NULL|   low| Ana - cherry|
+|  Ana|   fig|    goat|  NULL|  octagon|  high|   Ana - date|
+|  Ben| apple|     cat|   red|     NULL|  high|          Ben|
+|  Ben|banana|    NULL|  blue|   circle|medium|  Ben - apple|
+|  Ben|cherry|     dog|  NULL| triangle|   low| Ben - banana|
+|  Ben| grape|   horse|orange|rectangle|medium| Ben - cherry|
+|Marta| apple|  iguana|  pink|     NULL|   low|        Marta|
+|Marta| apple|  jaguar| black|  diamond|   low|Marta - apple|
+|Marta|cherry|     fox|purple|  hexagon|   low|Marta - apple|
++-----+------+--------+------+---------+------+-------------+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
